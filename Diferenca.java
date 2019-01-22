f59e4adf51c054e6917028e672acb9c6ad4d1409
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-01-22 14:16:46 -0200

    Resolve o exercicio URI1007

---
+++ Diferenca.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Diferenca {
+	 public static void main(String[] args) {
+	        Scanner numero = new Scanner(System.in);
+	        int A = numero.nextInt();
+	        int B = numero.nextInt();
+	        int C = numero.nextInt();
+	        int D = numero.nextInt();
+
+
+	        System.out.println("DIFERENCA = " + ((A * B) - (C * D)));
+	    }
+}
