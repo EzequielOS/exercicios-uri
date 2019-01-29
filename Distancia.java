2cca49c55d747454be17d341843808877430cb84
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-01-29 13:26:26 -0200

    Resolve o exercício URI1016

---
+++ Distancia.java
@@ -0,0 +1,20 @@
+/*
+ Nome do autor: Ezequiel de Oliveira Sant'Ana
+Data de criacao do  arquivo: 29/01/2019
+Objetivo sucinto do arquivo: Resolver exercicios do URI
+Referencia: URI Online Judge
+Informacoes adicionais: 
+
+ */
+import java.util.Scanner;
+
+
+public class Distancia {
+	public static void main(String[] args) {
+		int distanciaKM = new Scanner(System.in).nextInt();
+		
+		int calculo1 = distanciaKM *2;
+		
+		System.out.println(calculo1 + " minutos");
+	}
+}
